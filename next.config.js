2a1b31694b8359696c04ecab514ceceaccaf7b26
mikule1 <mikule1@gmail.com>
2024-10-27 19:21:14 +0200

    Fix paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: './', // Добавьте эту строку
+  basePath: '',      // Очистите basePath
+  trailingSlash: true // Добавьте эту строку
 }
 
 module.exports = nextConfig
