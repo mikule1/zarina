76ec1cdf0140e539cf7120ec42f95c60e70c10d0
mikule1 <mikule1@gmail.com>
2024-10-29 12:13:27 +0200

    Fix basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const { i18n } = require('./next-i18next.config')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
   images: { unoptimized: true },
-  basePath: '',
-  assetPrefix: '',
+  basePath: isProduction ? '/zarina' : '',
+  assetPrefix: isProduction ? '/zarina/' : '',
 }
 
 module.exports = nextConfig
