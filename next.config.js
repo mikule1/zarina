3db528d5c754e86ae5f904531567f5198940d03b
mikule1 <mikule1@gmail.com>
2024-10-29 11:57:54 +0200

    Fix i18n configuration and image paths

--- next.config.js
+++ next.config.js
@@ -3,10 +3,9 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  output: 'export',
   images: { unoptimized: true },
-  basePath: '/zarina',
-  assetPrefix: '/zarina/',
+  basePath: '',
+  assetPrefix: '',
 }
 
 module.exports = nextConfig
